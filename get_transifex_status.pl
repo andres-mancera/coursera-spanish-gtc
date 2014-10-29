60f4338d9dd06573a746693173f0dd67e5097bb6
andres-mancera <andres-mancera@users.noreply.github.com>
2014-10-29 16:23:24 -0700

    Addig code to generate a report file.

--- get_transifex_status.pl
+++ get_transifex_status.pl
@@ -29,10 +29,11 @@ my $user        = $options{user};
 my $pwd         = $options{pwd};
 my $config_file = $options{cfg_file};
 my (@config_file_info, @coursera_courses, @slugs);
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst);
 my ($course_details, $file_name, $i, $decoded_course_json, $decoded_resource_json);
 my ($slug_id, $resource_details, $translated_sum, $reviewed_sum, $untranslated_sum);
 my ($reviewed_strings, $translated_strings, $untranslated_strings);
-my ($translated, $reviewed);
+my ($translated, $reviewed, $course_name);
 
 # Open the configuration file and read all the information about the courses
 if ( open CONFIG_FILE, "$config_file" )
@@ -56,6 +57,15 @@ foreach ( @config_file_info )
 }
 print ("--Done!\n\n");
 
+# Open the report file and append a timestamp
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+$mday = sprintf("%02d", $mday);
+$mon = $mon + 1;  #$mon is in the range 0..11
+$mon = sprintf("%02d", $mon);
+$year += 1900; # $year is the number of years since 1900
+$file_name   = $year . "-" . $mon . "-" . $mday . "_status_report.txt";
+open (REPORT, ">$file_name") || die "Can't open new report file: $!\n";
+
 # Use Transifex's API to get information about the courses specified in the config file
 print ("Getting course details from Transifex...\n");
 for ( $i=0; $i<=$#coursera_courses; $i++ )
@@ -68,12 +78,6 @@ for ( $i=0; $i<=$#coursera_courses; $i++ )
   print ("  --> $coursera_courses[$i]\n");
   $course_details = 
     `curl -s -S -L -k --user '$user:$pwd' -X GET 'https://www.transifex.com/api/2/project/$coursera_courses[$i]/?details'`;
-  # The lines below have been commented out because the script no longer dumps the 
-  # output of the API call into a text file.
-  #$file_name = $coursera_courses[$i] . ".txt";
-  #open (COURSE_DETAILS, ">$file_name") || die "Can't open new file: $!\n";
-  #print COURSE_DETAILS @course_details;
-  #close COURSE_DETAILS
   $decoded_course_json = decode_json( $course_details );
   #print Dumper $decoded_course_json->{'resources'};    # DEBUG only!
   @slugs = @{ $decoded_course_json->{'resources'} };
@@ -95,6 +99,13 @@ for ( $i=0; $i<=$#coursera_courses; $i++ )
   $translated   = int(100*$translated_sum/($translated_sum+$untranslated_sum));
   $reviewed     = int(100*$reviewed_sum/($translated_sum+$untranslated_sum));
   print ("    --> Course Total :: translated=$translated :: reviewed=$reviewed\n");
-
+  $course_name  = $decoded_course_json->{'name'};
+  print REPORT "COURSE NAME : $course_name\n";
+  print REPORT "\tTRANSLATED : $translated%\n";
+  print REPORT "\tREVIEWED   : $reviewed%\n\n";
 }
+print ("--Done!\n\n");
+
+print ("Generating status report : $file_name\n");
+close REPORT;
 print ("--Done!\n\n");
